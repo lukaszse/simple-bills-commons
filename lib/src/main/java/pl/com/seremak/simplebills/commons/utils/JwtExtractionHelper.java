d9601372208c9dd6bc08e370890c32378124d726
lseremak <llseremak@gmail.com>
2022-11-13 19:17:24 +0100

    Version 0.0.7

--- lib/src/main/java/pl/com/seremak/simplebills/commons/utils/JwtExtractionHelper.java
+++ lib/src/main/java/pl/com/seremak/simplebills/commons/utils/JwtExtractionHelper.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import pl.com.seremak.simplebills.commons.dto.http.TokenUser;
 
 import java.util.Map;
+import java.util.Objects;
 
 import static pl.com.seremak.simplebills.commons.utils.ObjectMapperUtils.objectMapper;
 
@@ -13,6 +14,7 @@ import static pl.com.seremak.simplebills.commons.utils.ObjectMapperUtils.objectM
 public class JwtExtractionHelper {
 
     public static final String EXTRACTING_TOKEN_ERROR_MSG = "Error while extracting token. Reason: %s";
+    public static final String TOKEN_NOT_MATCH = "Token not match";
 
 
     public static String extractUsername(final JwtAuthenticationToken jwtAuthenticationToken) {
@@ -26,6 +28,13 @@ public class JwtExtractionHelper {
             });
         } catch (final Exception e) {
             final String errorMsg = EXTRACTING_TOKEN_ERROR_MSG.formatted(e.getMessage());
+            throw new AuthenticationServiceException(errorMsg);
+        }
+    }
+
+    public static void validateUsername(final String tokenUsername, final String bodyUsername) {
+        if (Objects.nonNull(tokenUsername) && tokenUsername.equals(bodyUsername)) {
+            final String errorMsg = EXTRACTING_TOKEN_ERROR_MSG.formatted(TOKEN_NOT_MATCH);
             throw new AuthenticationServiceException(errorMsg);
         }
     }
