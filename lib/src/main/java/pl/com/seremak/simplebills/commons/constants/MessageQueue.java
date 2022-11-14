21378d715e007adba5dee21c1cdcf6f7a6b3ac2c
lseremak <llseremak@gmail.com>
2022-11-14 21:44:34 +0100

    Version 0.0.25

---
+++ lib/src/main/java/pl/com/seremak/simplebills/commons/constants/MessageQueue.java
@@ -0,0 +1,10 @@
+package pl.com.seremak.simplebills.commons.constants;
+
+public class MessageQueue {
+
+    public static final String USER_CREATION_QUEUE = "userCreation";
+    public static final String CATEGORY_EVENT_QUEUE = "categoryEvent";
+    public static final String TRANSACTION_EVENT_QUEUE = "transactionEvent";
+    public static final String TRANSACTION_CREATION_REQUEST_QUEUE = "transactionCreation";
+
+}
