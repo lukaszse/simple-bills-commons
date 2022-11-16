4e5553b298c77765b998aca131bdb00cc71a2d04
lseremak <llseremak@gmail.com>
2022-11-16 21:52:47 +0100

    Version 0.0.28

--- lib/src/main/java/pl/com/seremak/simplebills/commons/model/Category.java
+++ lib/src/main/java/pl/com/seremak/simplebills/commons/model/Category.java
@@ -1,13 +1,10 @@
 package pl.com.seremak.simplebills.commons.model;
 
-import com.mongodb.lang.Nullable;
 import lombok.*;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.FieldType;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 
 @Getter
@@ -26,23 +23,17 @@ public class Category extends VersionedEntity {
         INCOME, EXPENSE
     }
 
-    @NotNull(message = "Username cannot be blank")
     private String username;
 
-    @NotBlank(message = "Name of Category cannot be blank")
     private String name;
 
-    @Nullable
     private Type type;
 
-    @NotNull(message = "Transaction type cannot be null")
     private TransactionType transactionType;
 
-    @Nullable
     @Field(targetType = FieldType.DECIMAL128)
     private BigDecimal limit;
 
-    @Nullable
     @Field(targetType = FieldType.DECIMAL128)
     private BigDecimal usageOfLimit;
 }
