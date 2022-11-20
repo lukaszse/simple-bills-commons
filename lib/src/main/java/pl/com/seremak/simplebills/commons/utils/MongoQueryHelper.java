b08701dc2f3f80254d4d2622e38a404a43dbfb8d
lseremak <llseremak@gmail.com>
2022-11-20 21:31:00 +0100

    Version 0.1.19

--- lib/src/main/java/pl/com/seremak/simplebills/commons/utils/MongoQueryHelper.java
+++ lib/src/main/java/pl/com/seremak/simplebills/commons/utils/MongoQueryHelper.java
@@ -2,7 +2,9 @@ package pl.com.seremak.simplebills.commons.utils;
 
 import org.springframework.data.mongodb.core.query.Update;
 
+import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.List;
 
 import static java.util.Objects.nonNull;
 import static pl.com.seremak.simplebills.commons.utils.ReflectionsUtils.getFieldValue;
@@ -12,9 +14,17 @@ public class MongoQueryHelper {
 
     public static <T> Update preparePartialUpdateQuery(final Object object, final Class<T> clazz) {
         final Update update = new Update();
-        Arrays.stream(clazz.getDeclaredFields())
+        getAllClassFields(clazz).stream()
                 .filter(field -> nonNull(getFieldValue(field, object)))
                 .forEach(field -> update.set(field.getName(), getFieldValue(field, object)));
         return updateMetadata(update);
+    }
+
+    private static <T> List<Field> getAllClassFields(Class<T> clazz) {
+        final List<Field> classFields = Arrays.asList(clazz.getDeclaredFields());
+        if(nonNull(clazz.getSuperclass())) {
+            classFields.addAll(getAllClassFields(clazz.getSuperclass()));
+        }
+        return classFields;
     }
 }
